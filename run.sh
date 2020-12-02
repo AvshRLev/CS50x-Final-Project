2d3a71de86569fd78266a48ccc83bfe838e9a384
AvshRLev <avsha.earth@gmail.com>
2020-12-02 12:54:10 +0200

    added app.py deleted name change of application.py to app.py fron run.sh, add installation of python3-pip to line 4 of setup.py, moved installation of python3-flask from run.sh to requirements.txt

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 pip3 install -r requirements.txt
-cp application.py app.py
-apt install -y python3-flask
 flask run 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #! \bin\bash
 
 apt-get update
-apt-get install -y git python3
+apt-get install -y git python3 python3-pip
 git clone https://github.com/AvshRLev/CS50x-Final-Project/
-apt-get install -y python3-pip
 ./run.sh
